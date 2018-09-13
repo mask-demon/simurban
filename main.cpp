170bc65c44796669e57edd2bc22448b8534c6d54
mask-demon <simu_ichou@yahoo.co.jp>
2018-09-13 21:52:29 +0900

    dassen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <algorithm>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include <SDL2/SDL.h>
 
-int windowWidth = 720, windowHight = 480;
+int windowWidth = 256, windowHight = 256;
 int hight = 256, width = 256;
 int interval = 100;
+const int minSquare = 4000, maxSquare = 10000;
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -32,11 +37,25 @@ int main(int argc, char *argv[]) {
 	SDL_Surface *surface = SDL_CreateRGBSurface(0, width, hight, 32, 0, 0, 0, 0);
 	SDL_Rect rect;
 	rect.h = rect.w = 1;
+	/*
 	for(int i = 0; i < 256; i++) {
 		rect.y = i;
 		for(int j = 0; j < 256; j++) {
 			rect.x = j;
 			SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, (i + 128) / 2, (j + 128) / 2, (255 - (i + j) / 2 + 128) / 2));
+		}
+	}
+	*/
+	for(int i = 0; i < 256; i++) {
+		rect.y = i;
+		for(int j = 0; j < 256; j++) {
+			rect.x = j;
+			int distSquare = (i * 2 - 255) * (i * 2 - 255) + (j * 2 - 255) * (j * 2 - 255);
+			if(maxSquare * 4 < distSquare) SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 255, 255));
+			else {
+				double ang = atan2(i * 2 - 255, j * 2 - 255);
+				SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, std::min(255, (int)(128 * (1 + sin(ang)))), std::min(255, (int)(128 * (1 + sin(ang + M_PI * 2 / 3)))), std::min(255, (int)(128 * (1 + sin(ang + M_PI * 4 / 3))))));
+			}
 		}
 	}
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
