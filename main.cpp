0dc103d63d58b0acd7166a9dc1df8c5d3d266ab1
mask-demon <simu_ichou@yahoo.co.jp>
2018-10-10 23:17:53 +0900

    iroiro

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@
 
 int windowWidth = 256, windowHight = 256;
 int hight = 256, width = 256;
+const int unitLength = 256;
 int interval = 100;
-const int minSquare = 4000, maxSquare = 10000;
+const int numLevel = 15;
+const int sizes[numLevel] = {16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512, 768, 1024, 1536, 2048};
+
+const int maxSquare = 10000;
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -28,6 +32,32 @@ int init() {
 		return 2;
 	}
 	return 0;
+}
+
+void my_RenderCopy(SDL_Renderer *R, SDL_Texture *T, const int Y, const int X, const int S) { //(Y, X): coordination of the center
+	if(S < 0 || S >= numLevel) return;
+	SDL_Rect rectR, rectT;
+	int rh, rw, th, tw;
+	SDL_GetRendererOutputSize(R, &rw, &rh);
+	SDL_QueryTexture(T, 0, 0, &tw, &th);
+	
+	//vertical
+	rectT.y = Y - rh * unitLength / sizes[S] / 2;
+	rectT.h = rh * unitLength / sizes[S];
+	if(Y * sizes[S] * 2 >= rh * unitLength) rectR.y = 0;
+	else rectR.y = rh / 2 - Y * sizes[S] / unitLength;
+	if((th - Y) * sizes[S] * 2 >= rh * unitLength) rectR.h = rh - rectR.y;
+	else rectR.h = rh / 2 + (th - Y) * sizes[S] / unitLength - rectR.y;
+	
+	//horizontal
+	rectT.x = X - rw * unitLength / sizes[S] / 2;
+	rectT.w = rw * unitLength / sizes[S];
+	if(X * sizes[S] * 2 >= rw * unitLength) rectR.x = 0;
+	else rectR.x = rw / 2 - X * sizes[S] / unitLength;
+	if((tw - X) * sizes[S] * 2 >= rw * unitLength) rectR.w = rw - rectR.x;
+	else rectR.w = rw / 2 + (tw - X) * sizes[S] / unitLength - rectR.x;
+
+	SDL_RenderCopy(R, T, &rectT, &rectR);
 }
 
 int main(int argc, char *argv[]) {
@@ -59,22 +89,29 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-	int h, w;
-	SDL_GetRendererOutputSize(renderer, &w, &h);
-	rect.y = rect.x = 0;
-	rect.h = rect.w = std::min(h, w);
-	SDL_RenderCopy(renderer, texture, 0, &rect);
-	SDL_RenderPresent(renderer);
 
 	SDL_Event event;
+	int size = 8;
+	int ypos = 128, xpos = 128;
 	while(true) {
 		while(SDL_PollEvent(&event)) {
+			bool rewrite = false;
 			if(event.type == SDL_QUIT) return 0;
-			if(event.type == SDL_WINDOWEVENT) {
-				SDL_GetRendererOutputSize(renderer, &w, &h);
-				rect.y = rect.x = 0;
-				rect.h = rect.w = std::min(h, w);
-				SDL_RenderCopy(renderer, texture, 0, &rect);
+			if(event.type == SDL_MOUSEWHEEL) {
+				size -= event.wheel.y;
+				if(size >= numLevel) size = numLevel - 1;
+				if(size < 0) size = 0;
+				rewrite = true;
+			} else if(event.type == SDL_MOUSEMOTION) {
+				if(event.motion.state & SDL_BUTTON_LMASK) {
+					ypos -= event.motion.yrel * unitLength / sizes[size];
+					xpos -= event.motion.xrel * unitLength / sizes[size];
+					rewrite = true;
+				}
+			} else if(event.type == SDL_WINDOWEVENT) rewrite = true;
+			if(rewrite) {
+				SDL_RenderClear(renderer);
+				my_RenderCopy(renderer, texture, ypos, xpos, size);
 				SDL_RenderPresent(renderer);
 			}
 		}
