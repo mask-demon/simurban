73f9471ff7f4dab32d33801ed5c84cc6a974e370
mask-demon <simu_ichou@yahoo.co.jp>
2018-10-12 18:06:18 +0900

    motion

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC := g++
-CPPFLAGS := -std=gnu++14 -Wall
-LDLIBS := -lSDL2_image `sdl2-config --cflags --static-libs` -static
+CPPFLAGS := -std=gnu++14 -Wall -O2
+LDLIBS := -lSDL2_image -ljpeg -lpng -ltiff -lwebp -lz -llzma `sdl2-config --cflags --static-libs` -static
 #LDLIBS := -lSDL2_image -lSDL2_ttf -lSDL2_mixer -ljpeg -lpng -lz -ltiff -llzma -lwebp -lfreetype -lharfbuzz -lfreetype -lgraphite2 -lbz2 -l:libmpg123.dll.a -lFLAC -lvorbisfile -lvorbis -logg `sdl2-config --cflags --static-libs` -static
 
 all: main
--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 #include <algorithm>
 
 #define _USE_MATH_DEFINES
 #include <cmath>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 int windowWidth = 256, windowHight = 256;
 int hight = 256, width = 256;
 const int unitLength = 256;
-int interval = 100;
+const int iconSize = 32;
+const int interval = 40;
 const int numLevel = 15;
 const int sizes[numLevel] = {16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512, 768, 1024, 1536, 2048};
 
+double angularVelocity = 0.1;
 const int maxSquare = 10000;
 
 SDL_Window *window;
@@ -21,11 +25,13 @@ SDL_Renderer *renderer;
 
 int init() {
 	SDL_Init(SDL_INIT_VIDEO| SDL_INIT_EVENTS);
+	IMG_Init(IMG_INIT_PNG);
 	window = SDL_CreateWindow( "SimUrban", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHight, SDL_WINDOW_RESIZABLE);
 	if(!window) {
 		fprintf(stderr, "Could not create a window: %s\n", SDL_GetError());
 		return 1;
 	}
+	SDL_SetWindowMinimumSize(window, iconSize, iconSize);
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if(!renderer) {
 		fprintf(stderr, "Could not create a renderer: %s\n", SDL_GetError());
@@ -61,8 +67,6 @@ void my_RenderCopy(SDL_Renderer *R, SDL_Texture *T, const int Y, const int X, co
 }
 
 int main(int argc, char *argv[]) {
-	printf("Simurban.\n");
-	fflush(stdout);
 	if(init()) return 1;
 	SDL_Surface *surface = SDL_CreateRGBSurface(0, width, hight, 32, 0, 0, 0, 0);
 	SDL_Rect rect;
@@ -76,6 +80,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	*/
+	
 	for(int i = 0; i < 256; i++) {
 		rect.y = i;
 		for(int j = 0; j < 256; j++) {
@@ -89,13 +94,20 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+	
+	SDL_Surface *surfaceIconOn  = IMG_Load("icon/start_on.png"); 
+	SDL_Surface *surfaceIconOff = IMG_Load("icon/start_off.png"); 
+	SDL_Texture *textureIconOn  = SDL_CreateTextureFromSurface(renderer, surfaceIconOn);
+	SDL_Texture *textureIconOff = SDL_CreateTextureFromSurface(renderer, surfaceIconOff);
 
-	SDL_Event event;
 	int size = 8;
-	int ypos = 128, xpos = 128;
+	int ypos = hight / 2, xpos = width / 2;
+	bool startOn = false;
+	bool rewrite = true;
+	double phase = 0;
+	SDL_Event event;
 	while(true) {
 		while(SDL_PollEvent(&event)) {
-			bool rewrite = false;
 			if(event.type == SDL_QUIT) return 0;
 			if(event.type == SDL_MOUSEWHEEL) {
 				size -= event.wheel.y;
@@ -108,12 +120,48 @@ int main(int argc, char *argv[]) {
 					xpos -= event.motion.xrel * unitLength / sizes[size];
 					rewrite = true;
 				}
+			} else if(event.type == SDL_KEYUP) {
+				if(event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
+					startOn = !startOn;
+					rewrite = true;
+				}
 			} else if(event.type == SDL_WINDOWEVENT) rewrite = true;
 			if(rewrite) {
 				SDL_RenderClear(renderer);
 				my_RenderCopy(renderer, texture, ypos, xpos, size);
+				rect.y = rect.x = 0;
+				rect.w = rect.h = iconSize;
+				SDL_RenderCopy(renderer, startOn? textureIconOn: textureIconOff, 0, &rect);
 				SDL_RenderPresent(renderer);
+				rewrite = false;
 			}
+		}		
+		{
+			if(startOn) {
+				phase += angularVelocity;
+				rect.h = rect.w = 1;
+				for(int i = 0; i < 256; i++) {
+					rect.y = i;
+					for(int j = 0; j < 256; j++) {
+						rect.x = j;
+						int distSquare = (i * 2 - 255) * (i * 2 - 255) + (j * 2 - 255) * (j * 2 - 255);
+						if(maxSquare * 4 < distSquare) SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 255, 255));
+						else {
+							double ang = atan2(i * 2 - 255, j * 2 - 255) + phase;
+							SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, std::min(255, (int)(128 * (1 + sin(ang)))), std::min(255, (int)(128 * (1 + sin(ang + M_PI * 2 / 3)))), std::min(255, (int)(128 * (1 + sin(ang + M_PI * 4 / 3))))));
+						}
+					}
+				}
+				SDL_DestroyTexture(texture);
+				texture = SDL_CreateTextureFromSurface(renderer, surface);
+			}
+			SDL_RenderClear(renderer);
+			my_RenderCopy(renderer, texture, ypos, xpos, size);
+			rect.y = rect.x = 0;
+			rect.w = rect.h = iconSize;
+			SDL_RenderCopy(renderer, startOn? textureIconOn: textureIconOff, 0, &rect);
+			SDL_RenderPresent(renderer);
+			rewrite = false;
 		}
 		SDL_Delay(interval);
 	}
